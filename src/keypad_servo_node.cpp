f65a53fba42c5066202703018eab84bc1d8b58fe
haad1r <haadi.rattansi@gmail.com>
2026-03-11 14:36:34 -0400

    Refactor KeypadServoNode constructor and destructor

--- src/keypad_servo_node.cpp
+++ src/keypad_servo_node.cpp
@@ -9,50 +9,46 @@ class KeypadServoNode : public rclcpp::Node
 
 public:
 
-    KeypadServoNode()
-    : Node("keypad_servo_node"),
-      servo_(12,13)
-    {
-
-        if(gpioInitialise() < 0)
-        {
-            RCLCPP_FATAL(this->get_logger(),"pigpio initialization failed");
-            rclcpp::shutdown();
-        }
-
-        servo_.initialize();
-
-        subscription_ =
-        this->create_subscription<std_msgs::msg::String>(
-            "/keypad_command",
-            10,
-            std::bind(&KeypadServoNode::command_callback,this,_1)
-        );
-
-        RCLCPP_INFO(this->get_logger(),"Dual Servo Keypad Node Started");
+    KeypadServoNode::KeypadServoNode()
+: Node("keypad_servo_node"),
+  servo_(12, 13)
+{
+    if (gpioInitialise() < 0) {
+        RCLCPP_FATAL(this->get_logger(), "pigpio initialization failed");
+        rclcpp::shutdown(); 
+        return;             
     }
 
-    ~KeypadServoNode()
-    {
+    servo_.initialize();    
+    initialized_ = true;     
 
+    subscription_ = this->create_subscription<std_msgs::msg::String>(
+        "/keypad_command",
+        10,
+        std::bind(&KeypadServoNode::command_callback, this, std::placeholders::_1)
+    );
+
+    RCLCPP_INFO(this->get_logger(), "Dual Servo Keypad Node Started");
+}
+
+    KeypadServoNode::~KeypadServoNode()
+{
+    if (initialized_) {
         servo_.home();
-
         gpioTerminate();
     }
+}
 
 private:
 
     KeypadServo servo_;
 
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
-
+    bool initialized_ = false; 
     void command_callback(const std_msgs::msg::String::SharedPtr msg)
     {
-
         std::string command = msg->data;
-
         RCLCPP_INFO(this->get_logger(),"Executing keypad command: %s",command.c_str());
-
         for(char digit : command)
 {
 
@@ -63,7 +59,6 @@ private:
 
     rclcpp::sleep_for(std::chrono::milliseconds(250));
 }
-
         servo_.home();
     }
 };
@@ -72,11 +67,8 @@ int main(int argc,char **argv)
 {
 
     rclcpp::init(argc,argv);
-
     auto node = std::make_shared<KeypadServoNode>();
-
     rclcpp::spin(node);
-
     rclcpp::shutdown();
 
     return 0;
