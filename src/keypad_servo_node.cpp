ab549c2099117fa44f8baa32a571a5a7ccf767fe
haad1r <haadi.rattansi@gmail.com>
2026-03-11 13:11:25 -0400

    Clean up blank lines in keypad_servo_node.cpp
    Removed unnecessary blank lines in keypad_servo_node.cpp

--- src/keypad_servo_node.cpp
+++ src/keypad_servo_node.cpp
@@ -1,10 +1,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
-
 #include "hardware_team_robot/keypad_servo.hpp"
-
 #include <pigpio.h>
-
 using std::placeholders::_1;
 
 class KeypadServoNode : public rclcpp::Node
