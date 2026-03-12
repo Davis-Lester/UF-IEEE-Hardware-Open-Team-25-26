82dd675cd8ac5598ea662f879b735f700de90bbe
haad1r <haadi.rattansi@gmail.com>
2026-03-12 09:39:03 -0400

    Define GPIO pins for motor control in camera mount

--- include/hardware_team_robot/camera_mount_node.h
+++ include/hardware_team_robot/camera_mount_node.h
@@ -2,6 +2,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include <pigpio.h>
+#include <atomic>
 
 enum MountState
 {
@@ -18,31 +19,34 @@ public:
 
 private:
 
-    // GPIO Pins (Dont know which ones to use)
-    const int PIN_MOTOR_A = ;
-    const int PIN_MOTOR_B = ;
-    const int PIN_PWM = ;
+    // GPIO Pins (change if needed)
+    const int PIN_MOTOR_A = 17;
+    const int PIN_MOTOR_B = 27;
+    const int PIN_PWM = 18;
 
-    // Can change timings
+    // Motion timings
     const float MAX_TRAVEL_TIME = 2.0;
-
     const float TIME_STOWED = 0.0;
     const float TIME_DRIVE = 1.2;
     const float TIME_MAX = 2.0;
+
     const int MAX_PWM = 200;
 
+    // State
     float current_time_pos_;
     MountState current_state_;
+
+    bool pigpio_ready_;
+    std::atomic<bool> cancel_motion_;
+
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr cmd_sub_;
+
     void cmd_callback(const std_msgs::msg::String::SharedPtr msg);
 
     void move_to_time(float target_time);
-
     void set_direction(bool up);
-
     void ramp_motion(float duration);
 
     void stop_motor();
-
     void emergency_stop();
 };
